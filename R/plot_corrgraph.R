93dfc09d278d4dd3b1984774a419fcc366b98f4c
MrDomani <pmorgen99@gmail.com>
2020-03-14 12:57:05 +0100

    Creating skeleton of a vignette

---
+++ R/plot_corrgraph.R
@@ -0,0 +1,6 @@
+#' @export
+plot_corrgraph <- function(df){
+  warning('Package in experimental state. Use this function ONLY with dfs which ALL columns are numeric 
+          (i.e. calculating corelation with cor() makes sense).')
+  
+}
