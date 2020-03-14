c6a39bc0441cf267b2d286fba05b6f85c976ac0a
MrDomani <pmorgen99@gmail.com>
2020-03-14 15:19:56 +0100

    removed dplyr from Imports

--- R/plot_corrgraph.R
+++ R/plot_corrgraph.R
@@ -1,33 +1,32 @@
-#' @importFrom dplyr "%>%"
 #' @export
-plot_corrgraph <- function(df){
+plot_corrgraph <- function(df, cutoff = 0.15){
   warning('Package in experimental state. Use this function ONLY with dfs which ALL columns are numeric 
           (i.e. calculating corelation with cor() makes sense).')
   corelations <- cor(df)
   coldict <- setNames(1:ncol(df), colnames(df))
-  no_corelation_approx <- 0.15
+  
   negative_corelation_handler <- function(x) x
   
   nodes <- data.frame(id = coldict,
-                      label = names(coldict))
-  edges <- cor(df) %>% as.data.frame() %>%
-    tidyr::pivot_longer(cols = 1:ncol(df),
-                 names_to = "to",
-                 values_to = "strength") %>%
-    dplyr::mutate(from = rep(colnames(df), each = ncol(df))) %>%
-    dplyr::filter(from != to) %>%
-    dplyr::mutate(length = negative_corelation_handler(1.1 - abs(strength)) * 500,
-           hidden = abs(strength) < no_corelation_approx,
-           physics = !hidden,
-           color = dplyr::if_else(strength >= 0, 'blue', 'red'),
-           label = as.character(round(strength, 2)),
-           from = coldict[from],
-           width = abs(strength) * 2 + 2,
-           to = coldict[to]) %>%
-    dplyr::filter(from >= to)
+                      label = names(coldict),
+                      size = 20)
+  corelations <- as.vector(cor(df))
+  edges <- data.frame(corelations = corelations, 
+                      from = rep(1:ncol(df), each = ncol(df)),
+                      to = rep(1:ncol(df), times = ncol(df)),
+                      length = negative_corelation_handler(1.1 - abs(corelations)) * 500,
+                      # dashes = abs(corelations) < no_corelation_approx,
+                      # opacity = ifelse(abs(corelations) < no_corelation_approx,
+                      #                  0.3,
+                      #                  1),
+                      color = ifelse(abs(corelations) < cutoff,
+                                     'grey',
+                                     ifelse(corelations >= 0, 'blue', 'red')),
+                      label = as.character(round(corelations, 2)),
+                      width = abs(corelations) * 2,
+                      smooth = FALSE)
+  edges <- edges[edges$from < edges$to, ]
   net <- visNetwork::visNetwork(nodes, 
-                    dplyr::select(edges, -strength), height = 600, width = 1000) %>%
-    visNetwork::visNodes(size = 20) %>% 
-    visNetwork::visEdges(smooth = FALSE) # %>%
+                    edges[,-which(colnames(edges)=='corelations')], height = 600, width = 1000)
   net
 }
