5ad63d0c31dd8d66c5719d8f419141f8bcb8f743
MrDomani <pmorgen99@gmail.com>
2020-03-15 14:41:07 +0100

    added FIFA to vignette

--- vignettes/vignette.R
+++ vignettes/vignette.R
@@ -2,9 +2,16 @@
 knitr::opts_chunk$set(echo = TRUE)
 library(CorrGrapheR)
 library(dplyr)
+#.
 
 ## ----cars---------------------------------------------------------------------
 df <- as.data.frame(datasets::Seatbelts) %>%
   select(-law)
 plot_corrgraph(df)
+
+## ----fifa---------------------------------------------------------------------
+load(normalizePath('../data/fifa20.rda'))
+fifa20_selected <- fifa20[,c(4,5,7,8,11:13,17,25:26,45:78)]
+nums <- unlist(lapply(fifa20_selected, is.numeric))
+plot_corrgraph(fifa20_selected[,nums], cutoff = 0.3)
 
