def74e9f54f1044a221c8b7255f2dd3846f2929f
MrDomani <pmorgen99@gmail.com>
2020-03-23 13:41:21 +0100

    added create_corrgraph for explainer

--- R/create_corrgrapher.R
+++ R/create_corrgrapher.R
@@ -2,7 +2,7 @@
 #' 
 #' Create a CorrGrapheR object before passing it to \code{plot()}. 
 #' @importFrom stats cor
-#' @param df a \code{data.frame}, in which for all \code{numeric} columns calling \code{\link{cor}} makes sense.
+#' @param x a \code{data.frame}, in which for all \code{numeric} columns calling \code{\link{cor}} makes sense.
 #' @param cutoff a number. Corelations below this are treated as \strong{no} corelation. Edges corresponding to them will \strong{not} be included in the graph.
 #' @param method passed directly to \code{\link{cor}} function. 
 #' @param ... other parameters.
@@ -14,32 +14,77 @@
 #' @seealso \code{\link{plot.corrgrapher}}
 #' @rdname create_corrgrapher
 #' @export
-create_corrgrapher <- function(df, ...){
-  UseMethod("create_corrgrapher", df)
+
+create_corrgrapher <- function(x, ...){
+  UseMethod("create_corrgrapher")
 }
 
 #' @rdname create_corrgrapher
 #' @export
 
-create_corrgrapher.default <- function(df, 
+create_corrgrapher.explainer <- function(x,
+                                         cutoff = 0.2,
+                                         feature_importance = NULL,
+                                         method = c('pearson', 'kendall', 'spearman')){
+  
+  if(is.null(feature_importance)){
+    x_feat <- ingredients::feature_importance(x)
+  } else x_feat <- feature_importance
+  x_feat <- x_feat[x_feat$permutation == 0, ]
+  names(x_feat)[names(x_feat) %in% c('variable', 'dropout_loss', 'label')] <- c('label', 'size', 'model_label')
+  create_corrgrapher(x$data, 
+                     cutoff = cutoff,
+                     method = method,
+                     sizes = x_feat)
+}
+
+#' @rdname create_corrgrapher
+#' @export
+
+create_corrgrapher.default <- function(x, 
                                        cutoff = 0.2, 
                                        method = c('pearson', 'kendall', 'spearman'),
+                                       sizes = NULL,
                                        ...){
-  nums <- unlist(lapply(df, is.numeric))
-  df <- df[,nums]
-  corelations <- stats::cor(df, method = method)
+  # sizes - df z kolumną label i size
+  # normalize robi wektor o średniej i sd danym na wejściu
+  normalize <- function(v, out_mean = 0, out_sd = 1) (v - mean(v, na.rm = TRUE)) / sd(v, na.rm = TRUE) * out_sd + out_mean
+  
+  # opcja druga: skalujemy wektor do zbioru[10,50]
+  normalize2 <- function(v, 
+                         out_min = 10, 
+                         out_max = 50) {
+    v <- v - min(v)
+    v <- v / max(v) * out_max
+    v + out_min
+  }
+  
+  if(is.null(sizes)) sizes <- data.frame(size = rep(5 * sqrt(ncol(x)), ncol(x)),
+                                       label = colnames(x))
+  else sizes$size <- normalize(sizes$size, 
+                          out_mean = 5 * sqrt(ncol(x)),
+                          out_sd = sqrt(ncol(x)))
+  
+  nums <- unlist(lapply(x, is.numeric))
+  x <- x[,nums]
+  
+  nodes <- data.frame(id = 1:ncol(x),
+                      label = colnames(x),
+                      title = colnames(x))
+  nodes <- merge(nodes, sizes, 
+                 by.x = 'label',
+                 by.y = 'label')
+  
+  corelations <- stats::cor(x, method = method)
   
   negative_corelation_handler <- function(x) x
   
-  nodes <- data.frame(id = 1:ncol(df),
-                      label = colnames(df),
-                      title = colnames(df),
-                      size = 5 * sqrt(ncol(df)))
-  corelations <- as.vector(cor(df))
+  
+  corelations <- as.vector(cor(x))
   edges <- data.frame(corelations = corelations, 
-                      from = rep(1:ncol(df), each = ncol(df)),
-                      to = rep(1:ncol(df), times = ncol(df)),
-                      length = negative_corelation_handler(1.1 - abs(corelations)) * 50 * ncol(df),
+                      from = rep(1:ncol(x), each = ncol(x)),
+                      to = rep(1:ncol(x), times = ncol(x)),
+                      length = negative_corelation_handler(1.1 - abs(corelations)) * 50 * ncol(x),
                       hidden = abs(corelations) < cutoff,
                       # dashes = abs(corelations) < no_corelation_approx,
                       # opacity = ifelse(abs(corelations) < no_corelation_approx,
--- vignettes/vignette.R
+++ vignettes/vignette.R
@@ -13,6 +13,33 @@ create_corrgrapher(df) %>%
 ## ----fifa---------------------------------------------------------------------
 load(normalizePath('../data/fifa20.rda'))
 fifa20_selected <- fifa20[,c(4,5,7,8,11:13,17,25:26,45:78)]
+# Value is skewed. Will be much easier to model sqrt(Value).
+
+fifa20_selected$value_eur <- log10(fifa20_selected$value_eur)
+fifa20_selected$team_position <- factor(fifa20_selected$team_position)
+fifa20_selected <- na.omit(fifa20_selected)
+fifa20_selected <- fifa20_selected[fifa20_selected$value_eur > 0,]
+fifa20_selected <- fifa20_selected[!duplicated(fifa20_selected[,1]),]
+rownames(fifa20_selected) <- fifa20_selected[,1]
+fifa20_selected <- fifa20_selected[,-1]
+
+# create a gbm model
+
+set.seed(1313)
+library("gbm")
+# 4:5 are overall and potential, too strong predictors
+fifa_gbm <- gbm(value_eur ~ . , data = fifa20_selected[,-(4:5)], n.trees = 250, interaction.depth = 3)
+
+library("DALEX")
+fifa_gbm_exp <- explain(fifa_gbm, 
+                        data = fifa20_selected[, -6], 
+                        y = 10^fifa20_selected$value_eur, 
+                        predict_function = function(m,x) 
+                          10^predict(m, x, n.trees = 250))
+
+library("ingredients")
+fifa_feat <- ingredients::feature_importance(fifa_gbm_exp)
+
 create_corrgrapher(fifa20_selected, cutoff = 0.3) %>%
   plot()
 
