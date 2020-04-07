ef791343559aa7bd89d336eb2cbcc1f8268889d9
MrDomani <pmorgen99@gmail.com>
2020-04-07 17:31:36 +0200

    initiated correlations function

---
+++ R/correlations.R
@@ -0,0 +1,21 @@
+#' Calculate correlation coefficients
+#' 
+#' Calculate correlation coefficients between variables in a \code{data.frame} 
+#' using 3 different functions for 3 different possible pairs of vairables:
+#' \itemize{
+#' \item{numeric - numeric}
+#' \item{numeric - categorical}
+#' \item{categorical - categorical}}
+#' 
+#' @export
+
+calculate_correlations <- function(x,
+                                   num_num_f,
+                                   num_cat_f,
+                                   cat_cat_f){
+  # cor.test - przyjmuje x i y
+  # chisq.test - przyjmuje x i y
+  
+  
+  
+}
