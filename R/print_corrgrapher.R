adf2c397cdee573449b795b4f4273bf05ca9485d
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2020-05-02 01:05:56 +0200

    added print function

---
+++ R/print_corrgrapher.R
@@ -0,0 +1,17 @@
+#' Print S3 method
+#' 
+#' This method allows \code{corrgrapher} objects to be displayed nicely in RStudio viewer.
+#' 
+#' @param x An object of \code{corrgrapher} class. See \code{\link{corrgrapher}} function.
+#' @param ... Other parameters, passed directly to \code{save_to_html} function.
+#' If \code{x} was created from \code{explainer}, the plot will visualize partial dependency
+#' of the currently selected variable.
+#' In other case, the plot will visualize distribution of the variable.
+#' 
+#' @export
+print.corrgrapher <- function(x, ...){
+  tmpf <- paste0(tempfile(), ".html")
+  save_to_html(x, file = tmpf, overwrite = TRUE)
+  viewer <- getOption("viewer")
+  viewer(tmpf)
+}
